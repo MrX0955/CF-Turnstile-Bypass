1f875777edd9547b621eddd3e4933795bbeb6d1e
Empty <77683874+MrX0955@users.noreply.github.com>
2023-06-03 18:32:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import capmonster_python.turnstile
 
-capmonster = capmonster_python.turnstile.TurnstileTask("YOUR CAPMONSTER API KEY HERE")
-task_id = capmonster.create_task("WEBSITE URL", "WEBSITE CAPTCHA TOKEN")
+capmonster = capmonster_python.turnstile.TurnstileTask("API KEY") #Your Capmonster Solver api key token here.
+task_id = capmonster.create_task("WEBSITE URL", "WEBSITE CAPTCHA TOKEN") #Captcha Token will be like this: 0x4AAAAAAABFlgIgsFUIYIzA
 result = capmonster.join_task_result(task_id)
 print(result.get("token"))
